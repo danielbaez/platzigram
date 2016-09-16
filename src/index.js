b4d33a64567f4dc4c7177f8fe05697f641226f6e
Daniel Baez <daniel_bg19@hotmail.com>
2016-09-16 15:14:09 -0500

    ad signup and signin

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var page = require('page');
+
+require('./homepage');
+require('./signup');
+require('./signin');
+
+page();
+
+// var numeros = [455,3434,4343,4343,43];
+
+// // var numeromas1 = numeros.map(function(numero) {
+// // 	return numero + 1;
+// // });
+
+// // console.log(numeromas1);
+
+// var numeromas1 = numeros.map(n => n + 1)
+
+// console.log(numeromas1);
